9070da05fec9ef90c05afc61844b15e9fa18e156
lopo <lanjun@goeasy.io>
2019-02-11 17:41:55 +0800

    init

--- book/plugin.js
+++ book/plugin.js
@@ -1,12 +1,12 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
-        config["baidu-tongji"] = config["baidu-tongji"] || {};
+        config["baidu-hosts"] = config["baidu-hosts"] || {};
         var host = location.host;
-        if(config["baidu-tongji"].host != host){
+        if(config["baidu-hosts"].host != host){
             return
         }
         var hm = document.createElement('script');
-        hm.src =config["baidu-tongji"].url+'?' + config["baidu-tongji"].token;
+        hm.src =config["baidu-hosts"].url+'?' + config["baidu-hosts"].token;
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(hm, s);
     });
